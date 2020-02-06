64e63e907cc274814020d6a1869ac4e3c0b57b1d
doron davidowitz <doron@evisame.com>
2020-02-06 16:08:49 +0200

    box  stage one

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// var box = document.getElementsByClassName("box");
+
+// // Add Box animation 
+// setInterval(function () { box.classList.add("boxAnimation"); }, 3000);
+
+// // Remove the box animation after 3 seconds 
+// // setTimeout(function () { alert("Hello"); }, 3000);
