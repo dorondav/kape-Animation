0a1d6e21d7359e3247c10013895e2224fe2525c2
doron davidowitz <doron@evisame.com>
2020-02-06 18:54:24 +0200

    box animation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 window.onload = function () {
 
     var lid = document.querySelector(".lidTieContainer");
-
     // // Add Box animation 
     // setInterval(function () { box.classList.add("boxAnimation"); }, 3000);
 
     // // Remove the box animation after 3 seconds 
-    // setTimeout(function () {
-    //     lid.style.display = "none";
-    // }, 5500);
+    setTimeout(function () {
+        lid.style.visibility = "hidden";
+    }, 5000);
+
 }
